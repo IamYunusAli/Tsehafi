48203f605a4105275406df56071943c80a427420
iamyunusali <aliyunus291@gmail.com>
2022-11-23 19:48:59 +0300

    Add test to post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  subject {
+    user=User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
+    Post.new(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 2,
+    likes_counter: 2)
+}
+
+it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+end
+
+it 'given post have a valid Author' do
+    expect(subject.author).to be_valid
+end
+
+it 'The Subject title is correct' do
+    expect(subject.title).to be_eql('Hello')
+end
+
+it 'The subject comment counter is an Integer' do
+    expect(subject.comments_counter).to be_an(Integer)
+end
+end
