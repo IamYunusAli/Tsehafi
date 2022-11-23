ecba80458ff766ddcf44682596606460d5c29292
iamyunusali <aliyunus291@gmail.com>
2022-11-23 13:59:42 +0300

    Add validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   has_many :comments, foreign_key: 'post_id'
   after_save :update_posts_counter
+  validates :title , presence: true , length: { maximum: 250 }
+  validates : likes_counter, numericality: { greater_than_or_equal_to: 0 , only_integer: true }
+  validates : comments_counter, numericality: { greater_than_or_equal_to: 0 , only_integer: true }
 
   def recent_comments
     comments.order('created_at desc').limit(5)
