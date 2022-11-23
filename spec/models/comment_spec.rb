15338543d88c10583ce3b6267529401ab04b7e49
iamyunusali <aliyunus291@gmail.com>
2022-11-23 19:49:11 +0300

    Add test to comment

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject {
+    user=User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
+    post=Post.new(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 2,
+    likes_counter: 2)
+    Comment.new(author: user, post: post, text: 'This is my first comment')
+}
+
+it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+end
+
+it 'Subject is valid with valid Author attributes' do
+    expect(subject.author).to be_valid
+end
+
+it 'Subject is valid with valid Post attributes' do
+    expect(subject.post).to be_valid
+end
+
+end
