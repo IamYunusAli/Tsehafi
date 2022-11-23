f35cc7bc8025dbc9497e87e1e9614333dec6cd7d
iamyunusali <aliyunus291@gmail.com>
2022-11-23 13:59:53 +0300

    Add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
+  validates :name, presence: true
+  validate :post_counter, numericality: { greater_than_or_equal_to: 0 , only_integer: true }
 
   private
 
