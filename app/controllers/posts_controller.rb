1dae229aeb144c2d54cb9d70046ee1a7b8dd634c
iamyunusali <aliyunus291@gmail.com>
2022-11-29 18:09:45 +0300

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-  @user = User.find(params[:user_id])
-  @posts = Post.all.where(author_id: @user.id)
+    @user = User.find(params[:user_id])
+    @posts = Post.all.where(author_id: @user.id)
   end
 
   def show
