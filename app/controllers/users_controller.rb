87c8c038c78599ddb184c98a34e96ae9148e5f61
iamyunusali <aliyunus291@gmail.com>
2022-11-29 18:06:56 +0300

    add controller index and show functionalitites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @posts = @user.recent_posts
+  end
 end
